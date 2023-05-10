049e1f587c7220afceae7f3774eb10110d6a637d
Sungho <hurgj123kr@gmail.com>
2023-05-10 21:25:31 +0900

    front Grapql api and REST api migrate

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import fetch from "node-fetch";
 
 let dolls = [
     {
@@ -74,8 +75,32 @@ Mayn people create Dolls objects
     }
     type Query {
         allDolles: [Dolls!]!
-        allUsers: [User!]! 
+        allUsers: [User!]!
+        allMovies: [Movie!]! 
         doll(id: ID!): Dolls
+        movie(id: ID!): Movie
+    }
+    type Movie {
+      id: Int!
+      url: String!
+      imdb_code: String!
+      title: String!
+      title_english: String!
+      title_long: String!
+      slug: String!
+      year: Int!
+      rating: Float!
+      runtime: Int!
+      summary: String!
+      description_full: String!
+      synopsis: String!
+      yt_trailer_code: String!
+      language: String!
+      mpa_rating: String!
+      state: String!
+      date_uploaded: String!
+      date_uploaded_unix: Int!
+      genres: [String!]!
     }
 
     type Mutation{
@@ -99,6 +124,14 @@ const resolvers = {
     allUsers() {
       return users
     },
+    allMovies() {
+        return fetch("https://yts.mx/api/v2/list_movies.json")
+          .then((result) => result.json())
+          .then((json) => json.data.movies)   
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`).then((result) => result.json()).then((json) => json.data.movie)
+    }
   },
   Mutation: {
       postDolls(root, { name, userId }) {
